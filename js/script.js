52cc36f1349f731ca16f9471cde5143d69bb0a76
Rahul Changlani <rahul.changlani@gmail.com>
2017-07-09 07:31:38 +0530

    AJAX XMLHttpRequests

--- js/script.js
+++ js/script.js
@@ -80,3 +80,20 @@ function loadData() {
 };
 
 $('#form-container').submit(loadData);
+
+window.onload = function() {
+
+    var http = new XMLHttpRequest();
+
+    http.onreadystatechange = function() {
+        if(http.readyState == 4 && http.status == 200) {
+            console.log(JSON.parse(http.response));
+        }
+    };
+
+    http.open('GET', '../data/file.json', true);
+    http.send();
+
+    console.log('test');
+
+};
